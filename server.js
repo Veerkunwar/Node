f022f056c3f85b1f7e5a0c89c96be0b05e904f73
veerkunwar <veerkunwarsingh19296@gmail.com>
2024-07-24 16:36:40 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+let http = require("http");
+let fs = require("fs");
+
+http
+  .createServer((req, res)=>{
+   fs.readFile('response.html',(err, data)=>{
+    if (!err){
+        res.writeHead(200,{
+            "Content-Type":"text/html",
+
+        });
+        res.write(data);
+        return res.end();
+
+    } 
+    res.write("error while reading file");
+    return res.end();
+   })
+  })
+  .listen(8080);
